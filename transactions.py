f80a5fd487c790fc9a0c284c5d618eb58b407446
Stefan Ghenescu <stefan.ghenescu@softwire.com>
2026-03-12 15:27:21 +0200

    Finish "List all" and "List [Account]" commands

--- transactions.py
+++ transactions.py
@@ -31,9 +31,66 @@ class Account:
 def readCSV():
     with open(FILENAME, mode = 'r') as file:
         csvFile = csv.DictReader(file)
-        return csvFile
+        return list(csvFile)
     
+def listAll(accounts):
+    for name, account in accounts.items():
+        print("Account for " + name + " owes: " + str(round(account.owe, 2)) + " and is owed: " + str(round(account.othersOwed, 2)))
+
+def listAccount(accounts, name):
+    if name not in accounts:
+        print("Account not found!")
+        return
+    
+    print("Transactions for " + name + "'s account")
+    
+    account = accounts[name]
+
+    transactionIndex = 1
+    for transaction in account.transactions:
+        print(str(transactionIndex) + ": " + transaction.date + " " + transaction.narrative)
+        transactionIndex = transactionIndex + 1
+
+
 
 if __name__ == "__main__":
-    transactionsFile = readCSV()
+    accounts = {}
     
+    # read data from csv
+    transactionsFile = readCSV()
+
+    # extract information from the data
+    for row in transactionsFile:
+        fromPerson = row['From']
+        toPerson = row['To']
+        
+        # if not created, add new account
+        if fromPerson not in accounts:
+            accounts[fromPerson] = Account(fromPerson)
+        
+        if toPerson not in accounts:
+            accounts[toPerson] = Account(toPerson)
+
+        # Create transaction
+        transaction = Transaction(
+            date=row['Date'],
+            fromAccount=row['From'],
+            toAccount=row['To'],
+            narrative=row['Narrative'],
+            amount=row['Amount']
+        )
+
+        # Add transaction to the accounts
+        accounts[fromPerson].addTransaction(transaction)
+        accounts[toPerson].addTransaction(transaction)
+
+    while True:
+        command = input()
+        if command == "List all":
+            listAll(accounts)
+        elif command.startswith("List "):
+            accName = command[5:].strip()
+            listAccount(accounts, accName)
+        else:
+            print("Unknown command.")
+
